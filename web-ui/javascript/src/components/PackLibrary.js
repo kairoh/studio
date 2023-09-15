6636be4ebd64141b0eba2683ee1a7862a4a20976
misterbenj34 <github@merklen.fr>
2023-09-16 00:28:16 +0200

    Update PackLibrary.js 5

--- web-ui/javascript/src/components/PackLibrary.js
+++ web-ui/javascript/src/components/PackLibrary.js
@@ -506,7 +506,7 @@ class PackLibrary extends React.Component {
                                             <span>{group.packs[0].title || group.uuid}</span>&nbsp;
                                         </div>
                                         <div className="pack-thumb" title={group.packs[0].nightModeAvailable && t('library.nightMode')}>
-                                            <img src={group.packs[0].image || defaultImage} alt="" width="128" height="128" draggable={false} />
+                                            <img src={group.packs[0].image || defaultImage} alt="" width="128" height="128" draggable={true} />
                                             {group.packs[0].official && <div className="pack-ribbon"><span>{t('library.official')}</span></div>}
                                         </div>
                                     </div>
